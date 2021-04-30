d3a03323c62bd1881e3f6883859051eb12f414e4
Shalom Yiblet <shalom.yiblet@gmail.com>
2021-04-29 23:44:23 -0700

    feat: add test about duplicate rows

--- examples/simple_concatenation.sql
+++ examples/simple_concatenation.sql
@@ -1,4 +1,4 @@
 -- @endpoint simpleConcatenation
 -- @param summand1
 -- @param summand2
-select @summand1::TEXT || ' <> ' || @summand2::TEXT
+select @summand1::TEXT || ' <> ' || @summand2::TEXT as result, 'concatenated succesfully' as info

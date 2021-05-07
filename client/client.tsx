60b1b1a4d826f494ec9967b96d45a004fa770e32
Shalom Yiblet <shalom.yiblet@gmail.com>
2021-05-07 13:49:13 -0700

    feat: add minimal client

---
+++ client/client.tsx
@@ -0,0 +1,37 @@
+export type Resp<A> =
+    | {
+        status: "error";
+        endpoint: string;
+        message: string;
+    }
+    | {
+        status: "success";
+        endpoint: string;
+        data: A;
+    };
+
+export async function auth<A>(
+    endpoint: string,
+    payload: { [key: string]: any }
+): Promise<Resp<A>> {
+    return fetch(`/justsql/api/v1/auth`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ endpoint, payload }),
+    }).then((resp) => resp.json());
+}
+
+export async function query<A>(
+    endpoint: string,
+    payload: { [key: string]: any }
+): Promise<Resp<A>> {
+    return fetch(`/justsql/api/v1/query`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify([{ endpoint, payload }]),
+    }).then(async (resp) => (await resp.json())[0]);
+}

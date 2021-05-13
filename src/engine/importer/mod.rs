56743cb3393f8a9568a2f48c1995059e09a9dbb4
Shalom Yiblet <shalom.yiblet@gmail.com>
2021-05-12 19:43:33 -0700

    chore: remove AsNeeded importer
    turns out it wasn't needed after all

--- src/engine/importer/mod.rs
+++ src/engine/importer/mod.rs
@@ -5,7 +5,6 @@ pub mod module_collection;
 mod upfront;
 mod watching;
 
-pub use as_needed::AsNeededImporter;
 pub use importer::Importer;
 pub use upfront::UpfrontImporter;
 pub use watching::WatchingImporter;

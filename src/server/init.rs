8520f35ae8807aae2f04b856a14b009ced7e39d8
Shalom Yiblet <shalom.yiblet@gmail.com>
2021-05-19 07:42:16 -0700

    fix: ensure database is connected during pool initialization

--- src/server/init.rs
+++ src/server/init.rs
@@ -10,19 +10,20 @@ pub async fn connect_to_db(
     max_connections: Option<u32>,
 ) -> anyhow::Result<Pool<Postgres>> {
     info!("connecting to the database");
+    let database_url = config
+        .database
+        .url
+        .as_ref()
+        .and_then(|v| v.value())
+        .ok_or_else(|| anyhow!("must have database url set in config"))?;
     let pool = sqlx::postgres::PgPoolOptions::new()
         .connect_timeout(Duration::from_secs_f32(10f32))
         .max_connections(max_connections.unwrap_or(10u32))
-        .connect(
-            config
-                .database
-                .url
-                .as_ref()
-                .and_then(|v| v.value().map(|v| v.into_owned()))
-                .ok_or_else(|| anyhow!("must have database url set in config"))?
-                .as_str(),
-        )
+        .connect(database_url.as_str())
         .await?;
+    pool.acquire()
+        .await
+        .map_err(|_| anyhow!("failed to connect to database"))?;
     info!("succesfully connected to the database");
     Ok(pool)
 }

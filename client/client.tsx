f47ce54c5c6ae362253e9d15f8149448fe67e11b
Shalom Yiblet <shalom.yiblet@gmail.com>
2021-05-21 06:21:42 -0700

    feat: update client

--- client/client.tsx
+++ client/client.tsx
@@ -1,6 +1,12 @@
-// minimal client 
-// assumes justsql is routed to the /justsql path on the 
+// minimal client
+// assumes justsql is routed to the /justsql path on the
 // same endpoint as this system.
+
+// modify root to point to wherver your justsql endpoints are routed to.
+// for CORS you'll need to set ROOT to the domain.
+// Example if justsql is hosted at `justsql.api.example.com` set ROOT to `https://justsql.api.example.com/`
+// Or if justsql is proxied through `/justsql/:path*` then you should set root to be `/justsql/`
+const ROOT = "/";
 
 export type Resp<A> =
     | {
@@ -14,12 +20,25 @@ export type Resp<A> =
         data: A;
     };
 
-export async function auth<A>(
+function getFirst<A>(resp: Resp<A[]>): Resp<A> {
+    if (resp.status === "error") {
+        return resp;
+    } else {
+        return {
+            status: resp.status,
+            endpoint: resp.endpoint,
+            data: resp.data[0],
+        };
+    }
+}
+
+export async function auth(
     endpoint: string,
     payload: { [key: string]: any }
-): Promise<Resp<A>> {
-    return fetch(`/justsql/api/v1/auth`, {
+): Promise<Resp<string>> {
+    return fetch(`${ROOT}/api/v1/auth`, {
         method: "POST",
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
         },
@@ -30,12 +49,20 @@ export async function auth<A>(
 export async function query<A>(
     endpoint: string,
     payload: { [key: string]: any }
-): Promise<Resp<A>> {
-    return fetch(`/justsql/api/v1/query`, {
+): Promise<Resp<A[]>> {
+    return fetch(`${ROOT}/api/v1/query`, {
         method: "POST",
+        credentials: "include",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify([{ endpoint, payload }]),
     }).then(async (resp) => (await resp.json())[0]);
+}
+
+export async function queryFirst<A>(
+    endpoint: string,
+    payload: { [key: string]: any }
+): Promise<Resp<A>> {
+    return query<A>(endpoint, payload).then(getFirst);
 }
